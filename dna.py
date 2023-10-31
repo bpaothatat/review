89c39c2bf4c20612fd7caac66badc065426b2b3f
Benjamin Paothatat <bpaothat@gmail.com>
2023-10-30 19:15:05 -0700

    Add linear search

---
+++ dna.py
@@ -0,0 +1,26 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s): # don't run off end!
+            return gene
+        # initialize codon out of three nucleotides
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon) # add codon to gene
+    return gene
+
+my_gene: Gene = string_to_gene(gene_str)
+
+def linear_search(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+        return False
+    
