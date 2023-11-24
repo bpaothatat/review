e4cb01a5aff059fb6386b4c9a1d471985d6ed0bc
Benjamin Paothatat <bpaothat@gmail.com>
2023-11-24 08:19:45 -0700

    Finish missionaries

--- searching/missionaries.py
+++ searching/missionaries.py
@@ -17,7 +17,7 @@ class MCState:
                 "The boat is on the {} bank.").format(self.wm, self.wc, self.em, self.ec, ("west" if self.boat else "east"))
     
     def goal_test(self) -> bool: 
-        return self.is_legal and self.em == MAX_NUM and self.ex == MAX_NUM
+        return self.is_legal and self.em == MAX_NUM and self.ec == MAX_NUM
     
     @property
     def is_legal(self) -> bool:
@@ -53,3 +53,25 @@ class MCState:
                 success.append(MCState(self.wm + 1, self.wc + 1, not self.boat))
         return [x for x in success if x.is_legal]
     
+def display_solution(path: List[MCState]):
+    if len(path) == 0:
+        return 
+    old_state: MCState = path[0]
+    print(old_state)
+    for current_state in path[1:]:
+        if current_state.boat:
+            print("{} missionaries and {} cannibals moved from the east bank to the west bank.\n".format(old_state.em - current_state.em, old_state.ec - current_state.ec))
+        else:
+            print("{} missionaries and {} cannibals moved from the west bank to the east bank.\n".format(old_state.wm - current_state.wm, old_state.wc - current_state.wc))
+        print(current_state)
+        old_state = current_state
+
+
+if __name__ == "__main__":
+    start: MCState = MCState(MAX_NUM, MAX_NUM, True)
+    solution: Optional[Node[MCState]] = bfs(start, MCState.goal_test, MCState.successors)
+    if solution is None:
+        print("No solution found!")
+    else:
+        path: List[MCState] = node_to_path(solution)
+        display_solution(path)
