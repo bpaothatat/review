4364d6ec6a72998e2ffb3aadec246c4b5b2dc9a3
Benjamin Paothatat <bpaothat@gmail.com>
2023-11-29 21:14:00 -0700

    Add initial CSP

--- csp/csp.py
+++ csp/csp.py
@@ -11,3 +11,20 @@ class Constraint(Generic[V, D], ABC):
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         ...
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        self.variables: List[V] = variables
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a domain assigned to it.")
+            
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
